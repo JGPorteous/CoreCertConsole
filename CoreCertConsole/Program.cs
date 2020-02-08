391b758efd680b4ecebc0b04f83709210bfd419e
Justin Porteous <justin@porteous.co.za>
2020-02-08 17:11:41 +0200

    Moved reading test to TestRead class. Added CommandLineParser NuGet Dependency

--- CoreCertConsole/Program.cs
+++ CoreCertConsole/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
+
 
 namespace CoreCertConsole
 {
@@ -7,24 +7,11 @@ namespace CoreCertConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Certificate Store Enqury!\n");
-            foreach(StoreLocation storeLocation in (StoreLocation[])Enum.GetValues(typeof(StoreLocation)))
-            {
-                foreach (StoreName storeName in (StoreName[])Enum.GetValues(typeof(StoreName)))
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"\\\\{storeLocation.ToString()}\\{storeName.ToString()}");
+            //Test reading the Stores and Locations
+            TestRead testRead = new TestRead();
+            testRead.Run();
 
-                    using (var store = new X509Store(storeName, storeLocation))
-                    {
-                        store.Open(OpenFlags.ReadOnly);
-                        foreach (var cert in store.Certificates)
-                        {
-                            Console.WriteLine($"{cert.Subject.Substring(1,10)} : {cert.Thumbprint}");
-                        }
-                    }
-                }
-            }
+
         }
     }
 }
